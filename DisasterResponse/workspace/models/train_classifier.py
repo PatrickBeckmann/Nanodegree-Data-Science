4c5e9ed238fde4685fb3a9bbd2159485f8fd5176
Patrick Beckmann <Patrick.Beckmann@msg.group>
2023-01-17 08:23:01 +0100

    added Didaster Response Project. Cleaned structure

---
+++ DisasterResponse/workspace/models/train_classifier.py
@@ -0,0 +1,103 @@
+import sys
+import nltk
+nltk.download(['punkt', 'wordnet'])
+import pickle
+import pandas as pd 
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine(f'sqlite:///{database_filepath}')
+    print(engine)
+    df = pd.read_sql_table("DisasterCleaned", engine)
+    X = df["message"] # messages
+    Y = df[df.columns[4:]] #all 36 columns of categories
+    return X, Y, Y.columns.values
+
+def tokenize(text):
+    #Write a tokenization function to process your text data
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+            'vect__max_df': (0.5, 0.75), #20 minuten
+            'vect__min_df': (0.2, 0.4),
+            #'tfidf__smooth_idf': (True, False),
+            'clf__estimator__n_estimators': [20, 50],
+            'clf__estimator__criterion': ['gini', 'entropy']
+        }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    
+    y_pred = model.predict(X_test)
+    y_pred = y_pred.astype('int')
+    y_test = Y_test.astype('int')
+    
+    for index, column in enumerate(y_test):
+        print(classification_report(y_test.iloc[index], y_pred[index]), column)
+
+    
+
+def save_model(model, model_filepath):
+    pickle.dump(model,open(model_filepath,'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
